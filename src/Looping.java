8a3094472d1bbccd95aca4a715d99966c1d52981
MuhSatria <muh.satria0410@gmail.com>
2019-04-12 01:28:05 +0700

    belajar oop

---
+++ src/Looping.java
@@ -0,0 +1,8 @@
+public class Looping {
+    public static void main(String[] args) {
+        String[] teman = {"Erza", "Yuki", "Ai", "Lapis"};
+        for (int i = 0; i < teman.length; i++){
+            System.out.println("nama teman " + i + ": " + teman[i]);
+        }
+    }
+}
